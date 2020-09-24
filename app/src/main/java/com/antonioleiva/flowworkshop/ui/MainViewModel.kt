e4739f6e369ffe811a887f4d72fe91d24db7882a
Antonio Leiva <antonioleivag@gmail.com>
2020-09-25 01:36:58 +0200

    Use viewModelScope instead of the custom one
    (cherry picked from commit 34bc176da7a37e308d82b67f176a8d03992c59cf)

--- app/src/main/java/com/antonioleiva/flowworkshop/ui/MainViewModel.kt
+++ app/src/main/java/com/antonioleiva/flowworkshop/ui/MainViewModel.kt
@@ -3,17 +3,12 @@ package com.antonioleiva.flowworkshop.ui
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.antonioleiva.flowworkshop.data.domain.Movie
 import com.antonioleiva.flowworkshop.data.domain.MoviesRepository
-import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
+import kotlinx.coroutines.launch
 
-class MainViewModel(repository: MoviesRepository) : ViewModel(), CoroutineScope {
-
-    private val job = Job()
-
-    override val coroutineContext: CoroutineContext
-        get() = job + Dispatchers.Main
+class MainViewModel(repository: MoviesRepository) : ViewModel() {
 
     private val _spinner = MutableLiveData<Boolean>()
     val spinner: LiveData<Boolean> get() = _spinner
@@ -22,15 +17,10 @@ class MainViewModel(repository: MoviesRepository) : ViewModel(), CoroutineScope 
     val movies: LiveData<List<Movie>> get() = _movies
 
     init {
-        launch {
+        viewModelScope.launch {
             _spinner.value = true
             _movies.value = repository.getMovies()
             _spinner.value = false
         }
     }
-
-    override fun onCleared() {
-        job.cancel()
-    }
-
 }
